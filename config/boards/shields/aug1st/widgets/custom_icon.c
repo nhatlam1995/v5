f90715c67074138179e50630c8ee7e7435100d0c
Aug1st2023 <nhatlam1695@gmail.com>
2026-09-06 15:00:46 +0700

    OK

--- config/boards/shields/aug1st/widgets/custom_icon.c
+++ config/boards/shields/aug1st/widgets/custom_icon.c
@@ -3,9 +3,9 @@
 LV_IMG_DECLARE(icon);
 
 void aug1st_widget_icon_init(lv_obj_t *parent) {
-    lv_obj_t *img = lv_image_create(parent);
-    lv_image_set_src(img, &icon);
+    lv_obj_t *img = lv_img_create(parent);
+    lv_img_set_src(img, &icon);
     
-    // Đặt icon nằm chính giữa tuyệt đối trên màn hình 128x32
+    // Đặt icon nằm chính giữa màn hình 128x32
     lv_obj_align(img, LV_ALIGN_CENTER, 0, 0);
 }
