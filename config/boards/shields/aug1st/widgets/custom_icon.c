d4cd9452dec905b963917a0b981425bad76a1522
Aug1st2023 <nhatlam1695@gmail.com>
2026-09-06 15:27:29 +0700

    OK

--- config/boards/shields/aug1st/widgets/custom_icon.c
+++ config/boards/shields/aug1st/widgets/custom_icon.c
@@ -1,11 +1,10 @@
 #include <lvgl.h>
 
-LV_IMG_DECLARE(icon);
-
 void aug1st_widget_icon_init(lv_obj_t *parent) {
-    lv_obj_t *img = lv_img_create(parent);
-    lv_img_set_src(img, &icon);
-    
-    // Đặt icon nằm chính giữa màn hình 128x32
-    lv_obj_align(img, LV_ALIGN_CENTER, 0, 0);
+    lv_obj_t *box = lv_obj_create(parent);
+    lv_obj_set_size(box, 24, 10);
+    lv_obj_align(box, LV_ALIGN_CENTER, 0, 0);
+    lv_obj_set_style_bg_color(box, lv_color_black(), LV_PART_MAIN);
+    lv_obj_set_style_border_color(box, lv_color_white(), LV_PART_MAIN);
+    lv_obj_set_style_border_width(box, 1, LV_PART_MAIN);
 }
