e92526be014a76eb8192bc77198d75a00c69bd78
Aug1st2023 <nhatlam1695@gmail.com>
2026-09-06 19:13:16 +0700

    Test

--- config/boards/shields/aug1st/widgets/custom_icon.c
+++ config/boards/shields/aug1st/widgets/custom_icon.c
@@ -1,10 +1,9 @@
 #include <lvgl.h>
 
+LV_IMG_DECLARE(icon);
+
 void aug1st_widget_icon_init(lv_obj_t *parent) {
-    lv_obj_t *box = lv_obj_create(parent);
-    lv_obj_set_size(box, 24, 10);
-    lv_obj_align(box, LV_ALIGN_CENTER, 0, 0);
-    lv_obj_set_style_bg_color(box, lv_color_black(), LV_PART_MAIN);
-    lv_obj_set_style_border_color(box, lv_color_white(), LV_PART_MAIN);
-    lv_obj_set_style_border_width(box, 1, LV_PART_MAIN);
+    lv_obj_t *img = lv_img_create(parent);
+    lv_img_set_src(img, &icon);
+    lv_obj_align(img, LV_ALIGN_CENTER, 0, 0);
 }
